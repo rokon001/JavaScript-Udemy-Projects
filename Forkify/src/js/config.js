ffa8d3c27cb87daeeeaae255d98e5145eee58b74
rokon001 <75433444+rokon001@users.noreply.github.com>
2021-12-14 22:05:20 +0100

    Add files via upload

---
+++ Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dbe8c3e5-b37f-41a3-9a31-22f697a7b621';
+export const MODAL_CLOSE_SEC = 2.5;
